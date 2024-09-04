ea266983fa5efbfc5734aff213c5d8ecbf0f349a
AmanSingh-2003 <104725506+AmanSingh-2003@users.noreply.github.com>
2024-09-04 09:35:43 +0000

    save

--- basic.js
+++ basic.js
@@ -1,1 +1,115 @@
-console.log("Aman")
+// const accountId = 11435
+// let accountEmail = "hitesh@gmail.com"
+// var accountPassword = "jaipur"
+//  accountId = 444
+//  accountEmail = "aman@gmail.com"
+// console.table({accountId,accountEmail,accountPassword,})
+
+// Datatypes  
+// "use strict "
+// let name = "Aman"
+// let age = 18
+// let isLoggedIn = false
+
+// console.log(typeof undefined)
+// console.log(typeof "Aman")
+// console.log(typeof age)
+
+// let score = 33
+// console.log(typeof score)
+//    console.log(typeof (score))
+//    let score = "33"
+    //   let score = "33abc"
+//    console.log(typeof (score))
+//   let score = null
+//   console.log(typeof score)
+// let score = undefined
+// console.log(typeof score)
+//    let score = "33"
+//     let valueInNumber = Number(score)
+//     console.log(valueInNumber)
+
+
+// let score = "33abc"
+//     let valueInNumber = Number(score)
+//          console.log(valueInNumber)
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// let isLoggedIn = ""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn)
+  
+
+// let isLoggedIn = "Histesh"
+//  let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn)
+
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// // console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+//  operation 
+// let value = 3
+// let negValue = -value 
+// console.log(negValue)
+
+// let str1 = "hello"
+// let str2 = " hitesh"
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// console.log((3+4)*5 /3)
+// let x = 5;
+// let y = ++x
+// console.log(y)
+// console.log(x)
+
+
+// let x = 5;
+//  let y = x++
+//  console.log(y)
+//  console.log(x)
+
+// let numOne = 4
+// let numTwo = ++numOne
+// console.log(numTwo)
+
+// let numOne = 4
+// let numTwo = numOne++
+// console.log(numTwo)
+
+// comparsion opeartor
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)
+// let underdefined
+// console.log(underdefined==0)
+// console.log(underdefined>0)
+// console.log(underdefined<0)
+
+// let myYoutube = "Aman"
+// let anotherYoutube = myYoutube
+// anotherYoutube = "Raju"
+// console.log(anotherYoutube)
+// console.log(myYoutube)
+
+let user = {
+    email:"user@google.com",
+    upi: "user@ybl"
+}
+let userTwo = user
+userTwo.email = "aman@gmail.com"
+console.log(userTwo)
+console.log(user)
+
+
+
+
+
